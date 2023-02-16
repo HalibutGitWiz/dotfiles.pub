94a6b5fe86688859f9c55a50bca916c73b66ead5
tw <tw@pretexx.fr>
2023-02-16 19:53:41 +0100

    Fix to allow friendly snippets to work

--- nvim/.config/nvim/lua/custom/cmp.lua
+++ nvim/.config/nvim/lua/custom/cmp.lua
@@ -9,7 +9,7 @@ if not snip_status_ok then
 end
 
 -- What is this ??
--- require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load()
 
 -- Function required for improving the use of Tab in snippets
 local check_backspace = function()
@@ -108,8 +108,8 @@ cmp.setup {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
         nvim_lua = "[Nvim_Lua]",
