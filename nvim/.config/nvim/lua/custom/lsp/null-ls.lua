5959827ec2ac6a9743de63fe39bc924335a03a28
tw <tw@pretexx.fr>
2023-02-26 15:51:24 +0100

    Neovim - Add Stylelint CSS linter

--- nvim/.config/nvim/lua/custom/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/custom/lsp/null-ls.lua
@@ -13,10 +13,16 @@ null_ls.setup {
   sources = {
 
     -- PHP
-    diagnostics.phpcs.with { extra_args = { '--standard=PSR12'} },
+    diagnostics.phpcs.with {
+        filetypes = {'php', 'js', 'css'},
+        extra_args = { '--standard=PSR12'}
+    },
     diagnostics.phpmd.with { extra_args = { vim.fn.expand("~/.config/nvim/lua/custom/lsp/settings/phpmd.xml")} },
     diagnostics.phpstan,
     formatting.phpcbf.with { extra_args = { '--standard=PSR12'} },
+
+    -- css
+    diagnostics.stylelint.with { extra_args = { '--config', vim.fn.expand("~/.config/stylelint/.stylelintrc")}},
 
     -- SQL
     diagnostics.sqlfluff.with { extra_args = { "--dialect", "mysql" }},
