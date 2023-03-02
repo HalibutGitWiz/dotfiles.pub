cd87be99c43ce367765de59d93e01f839a987720
tw <tw@pretexx.fr>
2023-03-02 11:38:26 +0100

    Neovim - Add Psalm as a PHP linter

--- nvim/.config/nvim/lua/custom/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/custom/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+    return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,24 +9,28 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup {
-  debug = false,
-  sources = {
+    debug = false,
+    sources = {
 
-    -- PHP
-    diagnostics.phpcs.with {
-        filetypes = {'php', 'js', 'css'},
-        extra_args = { '--standard=PSR12', '--config-set', 'php-version', '70333'}
+        -- PHP
+        diagnostics.php, -- Native PHP diagnostic tool
+        diagnostics.phpcs.with {
+            filetypes = { 'php', 'js', 'css' },
+            extra_args = { '--standard=PSR12', '--config-set', 'php-version', '70333' }
+        },
+        diagnostics.phpmd.with { extra_args = { vim.fn.expand("~/.config/phpmd/phpmd.xml") } },
+        diagnostics.phpstan,
+        diagnostics.psalm.with {
+            extra_args = { '--threads=2', '--diff'}
+        },
+        formatting.phpcbf.with { extra_args = { '--standard=PSR12' } },
+
+        -- JS & CSS
+        diagnostics.stylelint.with { extra_args = { '--config', vim.fn.expand("~/.config/stylelint/.stylelintrc.yml") } },
+        formatting.prettierd,
+
+        -- SQL
+        diagnostics.sqlfluff.with { extra_args = { "--dialect", "mysql" } },
+        formatting.sqlfluff.with { extra_args = { "--dialect", "mysql" } }
     },
-    diagnostics.phpmd.with { extra_args = { vim.fn.expand("~/.config/phpmd/phpmd.xml")} },
-    diagnostics.phpstan,
-    formatting.phpcbf.with { extra_args = { '--standard=PSR12'} },
-
-    -- CSS
-    diagnostics.stylelint.with { extra_args = { '--config', vim.fn.expand("~/.config/stylelint/.stylelintrc.yml")}},
-    formatting.prettierd,
-
-    -- SQL
-    diagnostics.sqlfluff.with { extra_args = { "--dialect", "mysql" }},
-    formatting.sqlfluff.with { extra_args = { "--dialect", "mysql" }}
-  },
 }
