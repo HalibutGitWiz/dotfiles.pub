b55d324b3b275cd2fc7f829c8154d9e6af1f0754
tw <tw@pretexx.fr>
2023-03-04 18:23:35 +0100

    Awesome - Add tag names + change default terminal emulator to Alacritty

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -61,19 +61,19 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
+    awful.layout.suit.fair,
     awful.layout.suit.corner.nw,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.floating,
+    --[[ awful.layout.suit.tile.bottom, ]]
+    --[[ awful.layout.suit.tile.top, ]]
+    --[[ awful.layout.suit.fair.horizontal, ]]
+    --[[ awful.layout.suit.spiral.dwindle, ]]
+    --[[ awful.layout.suit.max, ]]
+    --[[ awful.layout.suit.max.fullscreen, ]]
+    --[[ awful.layout.suit.magnifier, ]]
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
@@ -169,7 +169,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "", "", "", "", "", "", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
